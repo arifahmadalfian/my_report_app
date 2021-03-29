3cd1a67780b3a39b60293152d4252885a7e7b89f
arifahmadalfian <arifahmadalfian@gmail.com>
2021-03-29 17:44:09 +0700

    Firebase Crashlytics

--- app/src/main/java/com/arifahmadalfian/myreportapp/MainActivity.kt
+++ app/src/main/java/com/arifahmadalfian/myreportapp/MainActivity.kt
@@ -2,10 +2,19 @@ package com.arifahmadalfian.myreportapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import com.google.firebase.crashlytics.FirebaseCrashlytics
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val btnCrash = findViewById<Button>(R.id.btn_crash)
+        btnCrash.setOnClickListener {
+            FirebaseCrashlytics.getInstance().log("Clicked on button")
+            FirebaseCrashlytics.getInstance().setCustomKey("str_key", "some_data")
+            throw RuntimeException("Test Crash")
+        }
     }
 }
